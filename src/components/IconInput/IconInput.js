399ff5db38b3bd02eb2ab3e9359c1e027a9c4cf0
Stéfanie LOISELEUR <stloiseleur@ippon.fr>
2025-06-11 21:48:12 +0200

    Adds the IconInput solution

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -4,7 +4,62 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 
 import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import VisuallyHidden from "../VisuallyHidden";
+
+const Wrapper = styled.div`
+  position: relative;
+  color: ${COLORS.gray700};
+  font-family: 'Roboto', sans-serif;
+  font-size: var(--font-size);
+`;
+
+const SIZES = {
+  small: {
+    "--padding": "4px 4px 4px 24px",
+    "--font-size": `${14 / 16}rem`,
+    "--border-size": "1px",
+  },
+  large : {
+    "--padding": "8px 8px 7px 36px",
+    "--font-size": `${18/16}rem`,
+    "--border-size": "2px",
+  }
+}
+
+const InputElement = styled('input')`
+  padding: var(--padding);
+  border: none;
+  border-bottom: var(--border-size) solid black;
+  color: inherit;
+  font-size: var(--font-size);
+  font-weight: 700;
+  
+  ::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+
+  &:focus {
+    outline-offset: 2px;
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled(Icon)`
+  position: absolute;
+  pointer-events: none;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto 0;
+
+  ${InputElement}:hover + & {
+    color: ${COLORS.black};
+  }
+`
 
 const IconInput = ({
   label,
@@ -13,7 +68,13 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const styles = SIZES[size];
+  const labelId = `id-${Math.random().toString(36).slice(-6)}`;
+  return <Wrapper style={styles}>
+    <VisuallyHidden id={labelId}>{label}</VisuallyHidden>
+    <InputElement aria-labelledby={labelId} placeholder={placeholder} style={{ width: width }} />
+    <IconWrapper id={icon} size={size === "small" ? 16 : 24} />
+  </Wrapper>;
 };
 
 export default IconInput;
